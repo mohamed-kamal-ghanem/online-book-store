bfc6ea7919ce1b61e7f0600c47ffab94325cf0a2
mohamed-kamal-ghanem <moghanem2222@gmail.com>
2023-08-23 00:48:22 +0100

    first commit

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,20 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>We are an online book store</p>
+      <p>
+        <strong className="lorem">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit molestiae
+          quibusdam accusantium magni ipsum laboriosam error at praesentium
+          omnis molestias repellat recusandae architecto, illum vitae facere ab
+          quam voluptatum reiciendis!
+        </strong>
+      </p>
+    </section>
+  );
+};
+
+export default About;
